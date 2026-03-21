33a3265a996b13124c05ea1625294e8c0aaa9ae2
sungbbang <sinsa5133@gmail.com>
2026-03-21 22:26:13 +0900

    feat: getAuthUserWithProfile 함수 작성

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,4 +1,5 @@
 import { createClient } from '@/lib/supabase/server';
+import db from './db';
 
 export async function getAuthUser() {
   const supabase = await createClient();
@@ -7,4 +8,13 @@ export async function getAuthUser() {
   } = await supabase.auth.getUser();
 
   return user;
+}
+
+export async function getAuthUserWithProfile() {
+  const user = await getAuthUser();
+  if (!user) return null;
+
+  return db.profile.findUnique({
+    where: { supabaseId: user.id },
+  });
 }
